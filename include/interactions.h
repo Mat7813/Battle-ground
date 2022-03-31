fc74214d5beccc418967d0601f7c307a948dfd38
Attadeurtia <Attadeurtia@hotmail.com>
2022-03-31 14:19:11 +0200

    void gestion_ligne_entite(t_wave *vague) -> void gestion_ligne_entite(t_wave *vague, int camp);

--- include/interactions.h
+++ include/interactions.h
@@ -5,11 +5,11 @@
 #include "vague.h"
 
 void attaque_entites(t_wave *vague_ennemie, t_wave *vague_joueur, joueur *player);
-void gestion_environnement(t_wave *vague_ennemie,t_wave *vague_joueur, joueur *player, SDL_Renderer *rendu);
+void gestion_environnement(t_wave *vague_ennemie, t_wave *vague_joueur, joueur *player, SDL_Renderer *rendu);
 tir *creer_tir(tir *t, int x, int y);
 tir *etat_tir(tir *t);
 void creer_defense(joueur *p, int x, int y, int degat, message *msg);
 defense *etat_defense(defense *def);
-void gestion_ligne_entite(t_wave *vague);
+void gestion_ligne_entite(t_wave *vague, int camp);
 int fichier_existe(char *nom);
 #endif
